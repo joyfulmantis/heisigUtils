63314befc6af0b653f583b995fccd91c4d887310
Nathan Maxson <joyfulmantis@gmail.com>
2014-11-25 18:42:38 +0200

    Ported to Data.Text

--- heisigUtils.hs
+++ heisigUtils.hs
@@ -1,12 +1,13 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.ByteString.Lazy       as BL
 import qualified Data.ByteString.Lazy.Char8 as BLC
-import qualified Data.ByteString.UTF8       as BU
-import           Data.Char
 import qualified Data.Csv                   as C
 import           Data.Foldable
-import           Data.List.Split            (splitOn)
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as TE
+import qualified Data.Text.IO               as TIO
 import           GHC.Generics
 import           Options.Applicative
 import           System.Exit
@@ -15,20 +16,20 @@ import           System.IO
 data Options = Options { _fname   :: FilePath
                        , _command :: Command }
 
-data Command = Dependencies String
-             | Search String
+data Command = Dependencies T.Text
+             | Search T.Text
              | Verify
              | Interactive
 
 data Heisig = Heisig
-  { rtID           :: String
-  , rtCharacter    :: String
-  , rtPOS          :: String
-  , rtKeyword      :: String
-  , rtReading      :: String
-  , rtLesson       :: String
-  , rtDependencies :: String
-  , rtAlias        :: String }
+  { rtID           :: T.Text
+  , rtCharacter    :: T.Text
+  , rtPOS          :: T.Text
+  , rtKeyword      :: T.Text
+  , rtReading      :: T.Text
+  , rtLesson       :: T.Text
+  , rtDependencies :: T.Text
+  , rtAlias        :: T.Text }
  deriving (Generic, Show)
 
 instance C.FromNamedRecord Heisig
@@ -41,7 +42,7 @@ main = do options <- execParser (parserOptions `withInfo` parserInfo)
             Left e -> putStrLn e
             Right r -> case _command options of
                          Interactive -> uncurry interactiveRun r
-                         z -> putStr $ uncurry (run z) r
+                         z -> TIO.putStr $ uncurry (run z) r
   where parserInfo = "A simple program which provides search and dependency listing for characters in Heisig's books"
 
 parserOptions :: Parser Options
@@ -64,11 +65,11 @@ parserCommand = subparser $
 
 parserDependencies :: Parser Command
 parserDependencies =
-  Dependencies <$> argument str (metavar "<Characters and/or Keywords>")
+  Dependencies <$> T.pack <$> strArgument (metavar "<Characters and/or Keywords>")
 
 parserSearch :: Parser Command
 parserSearch =
-  Search <$> argument str (metavar "<Characters and/or Keywords>")
+  Search <$> T.pack <$> strArgument (metavar "<Characters and/or Keywords>")
 
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
@@ -77,10 +78,10 @@ parseCSV :: BLC.ByteString -> Either String (C.Header, [Heisig])
 parseCSV contents =  do (x, y) <- C.decodeByName contents
                         return (x, toList y)
 
-abstractInteractive :: String -> (String -> IO ()) -> IO ()
-abstractInteractive prompt inputHandler = do putStr prompt
+abstractInteractive :: T.Text -> (T.Text -> IO ()) -> IO ()
+abstractInteractive prompt inputHandler = do TIO.putStr prompt
                                              hFlush stdout
-                                             input <- getLine
+                                             input <- TIO.getLine
                                              inputHandler input
 
 interactiveRun :: C.Header -> [Heisig] -> IO ()
@@ -88,62 +89,63 @@ interactiveRun header database = abstractInteractive "> " handler
   where handler "dependencies" = interactiveDependencies header database
         handler "search" = interactiveSearch header database
         handler "exit" = exitWith ExitSuccess
-        handler _ = putStrLn helpMsg >> interactiveRun header database
+        handler _ = TIO.putStrLn helpMsg >> interactiveRun header database
         helpMsg = "please use one of 'dependencies search exit'"
 
 interactiveDependencies :: C.Header -> [Heisig] -> IO ()
 interactiveDependencies header database =
   abstractInteractive "dependencies> " handler
   where handler "exit" = interactiveRun header database
-        handler input = do putStr $ run (Dependencies input) header database
+        handler input = do TIO.putStr $ run (Dependencies input) header database
                            interactiveDependencies header database
 
 interactiveSearch :: C.Header -> [Heisig] -> IO ()
 interactiveSearch header database = abstractInteractive "search> " handler
   where handler "exit" = interactiveRun header database
-        handler input = do putStr $ run (Search input) header database
+        handler input = do TIO.putStr $ run (Search input) header database
                            interactiveSearch header database
 
-run :: Command -> C.Header -> [Heisig] -> String
+run :: Command -> C.Header -> [Heisig] -> T.Text
 run (Dependencies query) header =
-  formater header . dependencies (splitOn ", " query)
-run (Search query) header = formater header . search (splitOn ", " query)
+  formater header . dependencies (T.strip <$> T.splitOn ", " query)
+run (Search query) header =
+  formater header . search (T.strip <$> T.splitOn ", " query)
 run Verify _ = verify . reverse
 
-formater :: C.Header -> Either String [Heisig] -> String
-formater _ (Left e) = e ++ "\n"
+formater :: C.Header -> Either T.Text [Heisig] -> T.Text
+formater _ (Left e) = e `T.append` "\n"
 formater header (Right r) =
-  BU.toString $ BL.toStrict $ C.encodeByName header r
+ TE.decodeUtf8 $ BL.toStrict $ C.encodeByName header r
 
-dependencies :: [String] -> [Heisig] -> Either String [Heisig]
+dependencies :: [T.Text] -> [Heisig] -> Either T.Text [Heisig]
 dependencies (d:ds) database =
   do first <- findHeisig d database
      theRest <- dependencies (ds ++ parseDependencies first) database
      pure $ first : theRest
 dependencies [] _ = pure []
 
-search :: [String] -> [Heisig] -> Either String [Heisig]
+search :: [T.Text] -> [Heisig] -> Either T.Text [Heisig]
 search (x:xs) database =
   do first <- findHeisig x database
      theRest <- search xs database
      pure $ first : theRest
 search [] _ = pure []
 
-verify :: [Heisig] -> String
+verify :: [Heisig] -> T.Text
 verify (x:xs) =
   case dependencies (parseDependencies x) xs of
     Left e -> e
-    Right _ -> rtID x ++ "...\n" ++ verify xs
+    Right _ -> rtID x `T.append` "...\n" `T.append` verify xs
 verify [] = "Looks spiffy...\n"
 
-findHeisig :: String -> [Heisig] -> Either String Heisig
+findHeisig :: T.Text -> [Heisig] -> Either T.Text Heisig
 findHeisig x database = case find matcher database of
                           Nothing -> Left $ "Could not find " `T.append` x
                           Just y -> pure y
   where matcher z = rtCharacter z == x
-                    || map toLower (rtKeyword z) == map toLower x
+                    || T.toCaseFold (rtKeyword z) == T.toCaseFold x
 
-parseDependencies :: Heisig -> [String]
-parseDependencies Heisig {rtDependencies = []} = []
+parseDependencies :: Heisig -> [T.Text]
+parseDependencies Heisig {rtDependencies = ""} = []
 parseDependencies x @ Heisig {rtDependencies = y} =
-  filter (\z -> z /= rtKeyword x) $ splitOn ", " y
+  filter (\z -> z /= rtKeyword x) $ T.strip <$> T.splitOn ", " y
