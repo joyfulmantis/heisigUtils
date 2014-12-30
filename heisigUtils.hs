b097f85afc9b1bcb3782fccecb2e760936062a94
Nathan Maxson <joyfulmantis@gmail.com>
2014-12-30 20:28:28 +0200

    added verification to each run, and refactored

--- heisigUtils.hs
+++ heisigUtils.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Monad.Writer
 import qualified Data.ByteString.Lazy       as BL
 import qualified Data.ByteString.Lazy.Char8 as BLC
 import qualified Data.Csv                   as C
-import           Data.Foldable
+import           Data.Either
+import qualified Data.Foldable              as F
+import qualified Data.Set                   as S
 import qualified Data.Text                  as T
 import qualified Data.Text.Encoding         as TE
 import qualified Data.Text.IO               as TIO
 import           GHC.Generics
 import           Options.Applicative
+import           Prelude
 import           System.Exit
 import           System.IO
 
@@ -18,7 +22,6 @@ data Options = Options { _fname   :: FilePath
 
 data Command = Dependencies T.Text
              | Search T.Text
-             | Verify
              | Interactive
 
 data Heisig = Heisig
@@ -36,14 +39,19 @@ instance C.FromNamedRecord Heisig
 instance C.ToNamedRecord Heisig
 
 main :: IO ()
-main = do options <- execParser (parserOptions `withInfo` parserInfo)
-          contents <- BL.readFile (_fname options)
-          case parseCSV contents of
-            Left e -> putStrLn e
-            Right r -> case _command options of
-                         Interactive -> uncurry interactiveRun r
-                         z -> TIO.putStr $ uncurry (run z) r
+main =
+  do options <- execParser (parserOptions `withInfo` parserInfo)
+     contents <- BL.readFile (_fname options)
+     case parseCSV contents of
+       Left e -> putStrLn e >> failExit
+       Right r ->
+         let (k, l) = runWriter $ verify (snd r)
+         in ( TIO.putStr l >>
+              if k
+                then uncurry (run (_command options)) r
+                else failExit)
   where parserInfo = "A simple program which provides search and dependency listing for characters in Heisig's books"
+        failExit = exitWith $ ExitFailure 1
 
 parserOptions :: Parser Options
 parserOptions = Options <$> parserFile <*> parserCommand
@@ -54,29 +62,63 @@ parserFile = strOption $ short 'f' <> long "file" <> metavar "FILE"
 
 parserCommand :: Parser Command
 parserCommand = subparser $
-    command "dependencies" (parserDependencies `withInfo` depInfo)
-  <> command "search"      (parserSearch `withInfo` serInfo)
-  <> command "verify"      (pure Verify `withInfo` verInfo)
+    command "dependencies" (parserQueryer Dependencies `withInfo` depInfo)
+  <> command "search"      (parserQueryer Search `withInfo` serInfo)
   <> command "interactive" (pure Interactive `withInfo` interInfo)
   where depInfo = "Returns a csv of all the Heisig dependencies for the provided characters and keywords"
         serInfo = "Returns a csv of the all the entries for the provided characters and keywords"
-        verInfo = "Verifys that there are no missing dependencies in the csv file"
         interInfo = "Run's heisigUtils interactively"
 
-parserDependencies :: Parser Command
-parserDependencies =
-  Dependencies <$> T.pack <$> strArgument (metavar "<Characters and/or Keywords>")
-
-parserSearch :: Parser Command
-parserSearch =
-  Search <$> T.pack <$> strArgument (metavar "<Characters and/or Keywords>")
+parserQueryer :: (T.Text -> Command) -> Parser Command
+parserQueryer q = q <$> T.pack <$> strArgument (metavar "<Characters and/or Keywords>")
 
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
 parseCSV :: BLC.ByteString -> Either String (C.Header, [Heisig])
 parseCSV contents =  do (x, y) <- C.decodeByName contents
-                        return (x, toList y)
+                        return (x, F.toList y)
+
+verify :: [Heisig] -> Writer T.Text Bool
+verify xs = do a <- checkDependencies (reverse xs) (True, T.empty)
+               b <- checkDuplicates ys S.empty (True, T.empty)
+               return $ a && b
+  where ys = concatMap xToY xs
+        xToY Heisig {rtLesson = ""} = []
+        xToY z = [rtKeyword z] ++ parseCommas (rtAlias z)
+
+checkDuplicates :: [T.Text] -> S.Set T.Text -> (Bool, T.Text) -> Writer T.Text Bool
+checkDuplicates (x:xs) s a@(_, t) =
+  checkDuplicates xs (S.insert x s) a'
+  where a' = if S.member x s
+                 then ( False
+                      , "FATAL: Found duplicate "
+                        `T.append` x
+                        `T.append` "\n"
+                        `T.append` t)
+                 else a
+checkDuplicates [] _ a = writer a
+
+checkDependencies :: [Heisig] -> (Bool, T.Text) -> Writer T.Text Bool
+checkDependencies (x:xs)  a@(_, t) =
+  checkDependencies xs a'
+  where a' = case dependencies (parseDependencies x) xs of
+               Left e -> ( False
+                         , "FATAL: "
+                           `T.append` rtID x
+                           `T.append` ": "
+                           `T.append` e
+                           `T.append` "\n"
+                           `T.append` t)
+               Right _ -> a
+checkDependencies [] a = writer a
+
+run :: Command -> C.Header -> [Heisig] -> IO ()
+run (Dependencies query) header =
+  TIO.putStr . formater header . dependencies (parseCommas query)
+run (Search query) header =
+  TIO.putStr . formater header . search (parseCommas query)
+run Interactive header = interactiveRun header
 
 abstractInteractive :: T.Text -> (T.Text -> IO ()) -> IO ()
 abstractInteractive prompt inputHandler = do TIO.putStr prompt
@@ -96,26 +138,20 @@ interactiveDependencies :: C.Header -> [Heisig] -> IO ()
 interactiveDependencies header database =
   abstractInteractive "dependencies> " handler
   where handler "exit" = interactiveRun header database
-        handler input = do TIO.putStr $ run (Dependencies input) header database
-                           interactiveDependencies header database
+        handler input =
+          do run (Dependencies input) header database
+             interactiveDependencies header database
 
 interactiveSearch :: C.Header -> [Heisig] -> IO ()
 interactiveSearch header database = abstractInteractive "search> " handler
   where handler "exit" = interactiveRun header database
-        handler input = do TIO.putStr $ run (Search input) header database
+        handler input = do run (Search input) header database
                            interactiveSearch header database
-
-run :: Command -> C.Header -> [Heisig] -> T.Text
-run (Dependencies query) header =
-  formater header . dependencies (T.strip <$> T.splitOn ", " query)
-run (Search query) header =
-  formater header . search (T.strip <$> T.splitOn ", " query)
-run Verify _ = verify . reverse
 
 formater :: C.Header -> Either T.Text [Heisig] -> T.Text
 formater _ (Left e) = e `T.append` "\n"
 formater header (Right r) =
- TE.decodeUtf8 $ BL.toStrict $ C.encodeByName header r
+  TE.decodeUtf8 $ BL.toStrict $ C.encodeByName header r
 
 dependencies :: [T.Text] -> [Heisig] -> Either T.Text [Heisig]
 dependencies (d:ds) database =
@@ -131,21 +167,24 @@ search (x:xs) database =
      pure $ first : theRest
 search [] _ = pure []
 
-verify :: [Heisig] -> T.Text
-verify (x:xs) =
-  case dependencies (parseDependencies x) xs of
-    Left e -> e
-    Right _ -> rtID x `T.append` "...\n" `T.append` verify xs
-verify [] = "Looks spiffy...\n"
-
 findHeisig :: T.Text -> [Heisig] -> Either T.Text Heisig
-findHeisig x database = case find matcher database of
-                          Nothing -> Left $ "Could not find " `T.append` x
-                          Just y -> pure y
-  where matcher z = rtCharacter z == x
-                    || T.toCaseFold (rtKeyword z) == T.toCaseFold x
+findHeisig x database =
+  case F.find matcher database of
+    Nothing -> Left $ "Could not find: " `T.append` x
+    Just y -> pure y
+  where matcher z =
+          rtCharacter z == x
+          || T.toCaseFold (rtKeyword z) == T.toCaseFold x
+          || T.toCaseFold x `elem` (T.toCaseFold <$> parseCommas (rtAlias z))
 
 parseDependencies :: Heisig -> [T.Text]
-parseDependencies Heisig {rtDependencies = ""} = []
-parseDependencies x @ Heisig {rtDependencies = y} =
-  filter (\z -> z /= rtKeyword x) $ T.strip <$> T.splitOn ", " y
+parseDependencies x =
+  filter (\z -> isLeft (findHeisig z [x])) $ parseCommas (rtDependencies x)
+
+parseAliass :: Heisig -> [T.Text]
+parseAliass z = k <$> parseCommas (rtAlias z)
+  where k = T.append "#" . T.dropAround ('#' == )
+
+parseCommas :: T.Text -> [T.Text]
+parseCommas "" = []
+parseCommas y = T.strip <$> T.splitOn ", " y
